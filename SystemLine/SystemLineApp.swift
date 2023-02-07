66620680d19e383eb6e5914804c7136f1525b601
AtheerSaadA <atheerrsaad@gmail.com>
2023-02-07 12:08:34 +0300

    Initial Commit

---
+++ SystemLine/SystemLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemLineApp.swift
+//  SystemLine
+//
+//  Created by AtheerAlshehri on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SystemLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
